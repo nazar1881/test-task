c85bcc20aa13001c6df69e138a0a2360e4c8fb30
nazar1881 <90273320+nazar1881@users.noreply.github.com>
2022-11-17 05:59:58 -0500

    api changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,8 +3,5 @@ import axios from "axios";
 
 let dataUrl: string = "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu";
 
-export const getJobs = async () => {
-    const res = await axios.get<IJob[]>(dataUrl)
-    return res;
-}
+export const getJobs = async () => axios.get<IJob[]>(dataUrl)
 
